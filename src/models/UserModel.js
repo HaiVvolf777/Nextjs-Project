cb7e0a38ed7809627e7cb20c006256abd0870e17
Haider Ali <73600973+HaiVvolf777@users.noreply.github.com>
2023-07-06 19:03:16 +0500

    Create UserModel.js

---
+++ src/models/UserModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date
+
+});
+
+const User = mongoose.model.Users || mongoose.model("Users", userSchema);
+
+export default User;
