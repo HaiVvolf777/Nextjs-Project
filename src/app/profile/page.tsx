3c26419cb17cfc4088d1ab1a61836f7fcc4bd018
Haider Ali <73600973+HaiVvolf777@users.noreply.github.com>
2023-07-06 19:03:06 +0500

    Create page.tsx

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserProfilePage = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div>Profilepage</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfilePage;
