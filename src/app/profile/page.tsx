1d2ce99152fcaaa800e3e65f7085a6575423b860
Haider Ali <73600973+HaiVvolf777@users.noreply.github.com>
2023-07-07 18:52:30 +0500

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import React from "react";
+import React , {useState} from "react";
 import axios from "axios";
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 const UserProfilePage = () => {
+
+  const [data, setData] = useState("nothing")
   const router = useRouter();
 
   const logout = async () => {
@@ -20,17 +22,34 @@ const UserProfilePage = () => {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data);
+    setData(res.data.data._id)
+}
+
   return (
     <div className="container mx-auto">
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <div>Profilepage</div>
+      <div className="flex flex-col items-center justify-around min-h-screen py-2">
+        <div className="text-2xl font-bold">Profile Page</div>
+        <h2 className="p-1 rounded bg-red-500">{data === 'nothing' ? "Link" : <Link href={`/profile/${data}`}>{data}
+            </Link>}</h2>
         <hr />
+
+        <div className="flex gap-4 mt-5">
         <button
           onClick={logout}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
         >
           Logout
         </button>
+
+        <button
+        onClick={getUserDetails}
+        className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+        >GetUser Details</button>
+        </div>
+
       </div>
     </div>
   );
