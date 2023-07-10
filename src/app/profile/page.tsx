2341a12af1a8f4f4aa4b1a7c6f89b134192fef51
Haider Ali <73600973+HaiVvolf777@users.noreply.github.com>
2023-07-10 17:24:51 +0500

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -30,19 +30,19 @@ const UserProfilePage = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-between items-center h-[20vh] py-2">
-        <div className="text-2xl font-bold text-yellow-600">Profile Page</div>
+        <div className="text-2xl font-bold text-orange-500">Profile Page</div>
 
         <div className="flex gap-4 mt-5">
           <button
             onClick={logout}
-            className="bg-yellow-600 hover:bg-yellow-800 text-white font-bold py-2 px-4 rounded-lg"
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-lg"
           >
             Logout
           </button>
 
           <button
             onClick={getUserDetails}
-            className="bg-yellow-600  hover:bg-yellow-800 text-white font-bold py-2 px-4 rounded-lg"
+            className="bg-orange-500  hover:bg-orange-700 text-white font-bold py-2 px-4 rounded-lg"
           >
             GetUser Details
           </button>
@@ -50,12 +50,12 @@ const UserProfilePage = () => {
       </div>
 
       <div className="h-[80vh] flex justify-center items-center">
-      <div className="w-1/2 h-60 border flex justify-center items-center border-yellow-600 bg-yellow-600 bg-opacity-20 rounded-xl">
+      <div className="w-1/2 h-60 border flex justify-center items-center border-orange-800 bg-orange-500 bg-opacity-20 rounded-xl">
       <h2 className="p-1 rounded ">
         {data === "nothing" ? (
           "Link"
         ) : (
-          <Link className="bg-yellow-600 rounded-lg px-3 py-2" href={`/profile/${data}`}>{data}</Link>
+          <Link className="bg-orange-500 rounded-lg px-3 py-2" href={`/profile/${data}`}>{data}</Link>
         )}
       </h2>
       </div>
