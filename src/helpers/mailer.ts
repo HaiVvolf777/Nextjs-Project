387494e7b89d1a4d2b9bd8c37da0d682a6ee4599
Haider Ali <73600973+HaiVvolf777@users.noreply.github.com>
2023-07-10 17:03:14 +0500

    Update mailer.ts

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import User from "@/models/userModel";
+import User from "@/models/UserModel";
 import bcryptjs from "bcryptjs";
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
